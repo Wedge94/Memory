ebb3ec9d6d07f878ee67b42c40c913e33200944d
wedge94 <ozowedge@gmail.com>
2017-11-09 17:38:12 +0100

    Baza danych

---
+++ app/src/main/java/com/memory/dominik/benduski/memory/FeedReaderContract.java
@@ -0,0 +1,13 @@
+package com.memory.dominik.benduski.memory;
+
+import android.provider.BaseColumns;
+
+
+public final class FeedReaderContract
+{
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private FeedReaderContract() {}
+
+
+}
--- app/src/main/java/com/memory/dominik/benduski/memory/MainActivity.java
+++ app/src/main/java/com/memory/dominik/benduski/memory/MainActivity.java
@@ -1,48 +1,63 @@
 package com.memory.dominik.benduski.memory;
 
-import android.app.ActionBar;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
     private EditText numberOfPhotos;
     private final int minPhotos = 4;
     private final int maxPhotos = 10;
+    FeedReaderDbHelper mReaderDbHelper;
+
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button newGameButton = (Button) findViewById(R.id.newGame);
+        mReaderDbHelper = new FeedReaderDbHelper(this);
         newGameButton.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
                 numberOfPhotos = createEditTextView("Wpisz ilosc zdjec do zrobienia:");
                 newGameButton.setVisibility(View.GONE);
-                commitOnEditorActionListener();
+                commitOnEditorActionListener(mReaderDbHelper);
             }
         });
     }
 
-    private void commitOnEditorActionListener()
+    private void commitOnEditorActionListener(final FeedReaderDbHelper mReaderDbHelper)
     {
         numberOfPhotos.setOnEditorActionListener(new TextView.OnEditorActionListener()
         {
+
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent)
             {
-                toastMessage("TO ZYJE!");
+                int number = Integer.parseInt(textView.getText().toString());
+                if(number >= minPhotos && number <= maxPhotos)
+                {
+                    mReaderDbHelper.addData(Integer.toString(number));
+                    List l = mReaderDbHelper.getData();
+
+                    for(int j = 0; j < l.size(); j++)
+                    {
+                        toastMessage(l.get(j).toString());
+                    }
+                }
+                else
+                {
+                    toastMessage("Chuju kurwa zla liczba");
+                }
                 return false;
             }
         });
@@ -63,5 +78,11 @@ public class MainActivity extends AppCompatActivity
     private void toastMessage(String message)
     {
         Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onDestroy()
+    {
+        this.deleteDatabase("FeedReader.db");
+        super.onDestroy();
     }
 }
